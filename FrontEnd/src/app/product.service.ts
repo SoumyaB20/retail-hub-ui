c54e3ad7c892d02f0121402269abb42a4c4f2bc0
Nidhi-Billio <nidhi.t@brillio.com>
2023-08-09 19:45:34 +0530

    updates

--- FrontEnd/src/app/product.service.ts
+++ FrontEnd/src/app/product.service.ts
@@ -10,6 +10,7 @@ export class ProductService {
   private url="http://localhost:3000";
   constructor(private http:HttpClient) { }
 
+
   showProducts(): Promise<any> {
     return this.http.get(`${this.url}/products`).toPromise();
   }
--- FrontEnd/src/app/products/products.component.ts
+++ FrontEnd/src/app/products/products.component.ts
@@ -9,16 +9,26 @@ import { ProductService } from '../product.service';
 })
 export class ProductsComponent {
   product:Product= new Product();
-  list!:any;
-  constructor(private prodService: ProductService) {
-    
-  }
+  // list!:any;
+  allProducts: any[] = []; // Your array containing all products
+  electronicsProducts: any[] = [];
+  fashionProducts: any[] = [];
+  accessories: any[] = [];
+  kitchenware: any[] = [];
+  appliances: any[] = [];
+
+  constructor(private prodService: ProductService) {}
 
   async ngOnInit(): Promise<void> {
     try {
       const data = await this.prodService.showProducts();
       console.log(data);
-      this.list = data;
+      this.allProducts = data;
+      this.electronicsProducts = this.allProducts.filter(product => product.category === 'Electronics');
+      this.fashionProducts = this.allProducts.filter(product => product.category === 'Fashion');
+      this.appliances = this.allProducts.filter(product => product.category === 'Appliances');
+      this.kitchenware = this.allProducts.filter(product => product.category === 'Kitchenware');
+      this.accessories = this.allProducts.filter(product => product.category === 'Accessories');
     } catch (error) {
       console.error('An error occurred:', error);
   }}
